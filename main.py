b176dba3962bdc5f8a1780debce3a89529ced1e7
Rodrigo <rodrigito49@gmail.com>
2021-02-08 22:44:46 -0600

    Fase2

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import re
 import webbrowser
+from tkinter import filedialog
+
+
 
 memoria = []
 objectos = []
+lista_ayuda = []
+nuev_lista = []
+
+
 
 class Linea:
     def __init__(self, id, lista, instruccion1=False, instruccion2=None):
@@ -12,46 +19,90 @@ class Linea:
         self.instruccion2 = instruccion2
 
     def imprimir(self):
-        print(self.id)
-        print(self.lista)
-        print(self.instruccion1)
 
         if self.instruccion2 == None:
-            print('None')
+            print(str(self.id) + str(self.lista) + str(self.instruccion1))
         else:
-            print(self.instruccion2)
-
+            print(str(self.id) + str(self.lista) + str(self.instruccion1) + str(self.instruccion2))
+    """
     def ordenar(self):
+        lista1 = self.lista
+        #print(lista1)
         if self.instruccion1 == False:
             return
         else:
             cadena = ''
-            lista1 = self.lista
             for i in range(len(lista1)):
                 peque = min(lista1)
                 cadena += peque + ', '
                 lista1.remove(peque)
             return cadena
-
+    """
+    """
     def buscar(self):
+        nuev_lista = self.lista
+        #print(nuev_lista)
         if self.instruccion2 == None:
             return
         else:
             contador = 0
             validos = '{'
+            #print(self.lista)
             for elemento in self.lista:
                 contador += 1
                 if elemento == self.instruccion2:
                     validos = validos + str(contador)
-                    if contador < (len(self.lista) - 1):
+                    if contador < (len(nuev_lista) - 1):
                         validos += ','
                 else:
                     continue
             validos += '}'
             return validos
+    """
+
+def ordenar(Linea):
+    for elemao in Linea.lista:
+        lista_ayuda.append(elemao)
+    #lista_ayuda = Linea.lista
+    #print(lista1)
+    if Linea.instruccion1 == False:
+        return
+    else:
+        cadena = ''
+        for i in range(len(lista_ayuda)):
+            peque = min(lista_ayuda)
+            cadena += peque + ', '
+            lista_ayuda.remove(peque)
+        lista_ayuda.clear()
+        return cadena
+
+def buscar(Linea):
+    for elemao in Linea.lista:
+        nuev_lista.append(elemao)
+    if Linea.instruccion2 == None:
+        return
+    else:
+        contador = 0
+        validos = '{'
+        for elemento in Linea.lista:
+            contador += 1
+            if elemento == Linea.instruccion2:
+                validos = validos + str(contador)
+                if contador < (len(nuev_lista) - 1):
+                    validos += ','
+            else:
+                continue
+        validos += '}'
+        nuev_lista.clear()
+        if validos == '{}':
+            return 'Ninguna coincidencia'
+        else:
+            return validos
 
 """
-file = open('entrada.txt', 'r')
+#print(archivo.readlines())
+file = filedialog.askopenfile(title="abrir")
+#file = open('entrada.txt', 'r')
 for line in file:
     memoria.append(line.replace('\n', ''))
 print(memoria)
@@ -85,11 +136,13 @@ for com in memoria:
 
     a = Linea(id, lista, inst1, inst2)
     objectos.append(a)
-    a.imprimir()
-    #print(objectos)
+
+print(objectos)
+
 """
-"""
+
 while True:
+    print('\n')
     print('========Menu=========')
     print('1. Cargar archivo de entrada')
     print('2. Desplegar listas ordenadas')
@@ -105,7 +158,7 @@ while True:
         file = open('entrada.txt', 'r')
         for line in file:
             memoria.append(line.replace('\n', ''))
-        print(memoria)
+        #print(memoria)
         #print(len(memoria))
         #for com in memoria:
             #print(com)
@@ -136,40 +189,76 @@ while True:
             objectos.append(a)
 
     if comando == '2':
+        contador1 = -1
         for obj in objectos:
-            if obj.ordenar != None:
-                print(obj.id + ': ' + 'ORDENADOS = ' + obj.ordenar )
+            contador1 += 1
+            if obj.instruccion1 == False:
+                continue
+            elif obj.instruccion1 == True:
+                print(memoria[contador1] + ' | ' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(ordenar(obj)))
 
     if comando == '3':
+        contador2 = -1
         for obj in objectos:
-            if obj.buscar != None:
-                print(obj.id + ': ' + 'BUSQUEDA' + ' obj.instruccion2 = ' + obj.buscar )
+            contador2 += 1
+            if obj.instruccion2 != None:
+                print(memoria[contador2] + ' | ' + str(obj.id) + ': ' + 'BUSQUEDA ' + str(obj.instruccion2) + ' = ' + buscar(obj))
 
     if comando == '4':
+        contador3 = -1
         for obj in objectos:
-            if obj.ordenar != None:
-                print(obj.id + ': ' + 'ORDENADOS = ' + obj.ordenar )
-            elif obj.buscar != None:
-                print(obj.id + ': ' + 'BUSQUEDA' + ' obj.instruccion2 = ' + obj.buscar )
+            contador3 += 1
+            if obj.instruccion1 is True:
+                if obj.instruccion2 != None:
+                    print(memoria[contador3] + ' | ' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(ordenar(obj)) + 'BUSQUEDA ' + str(obj.instruccion2) + ' = ' + buscar(obj))
+                elif obj.instruccion2 == None:
+                    print(memoria[contador3] + ' | ' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(ordenar(obj)) )
+
+            elif obj.instruccion2 !=None:
+                print(memoria[contador3] + ' | ' + str(obj.id) + ': ' + 'BUSQUEDA ' + str(obj.instruccion2) + ' = ' + buscar(obj))
+
+            else:
+                continue
 
     if comando == '5':
         with open('reporte.html', 'w') as reporte:
-            reporte.write('<html>')
-            reporte.write('<head>')
-            reporte.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">')
-            reporte.write('<body>')
-            reporte.write('<center>')
+            reporte.write('<html>' + '\n')
+            reporte.write('<head>' + '\n')
+            reporte.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">' + '\n')
+            reporte.write('</head>' + '\n')
+            reporte.write('<body>' + '\n')
+            reporte.write('<center>' + '\n')
+            reporte.write('<table class="table table-hover table-striped">')
+            reporte.write('<thead>')
+            reporte.write('<tr>')
+            reporte.write('<th>No.</th>')
+            reporte.write('<th> Instruccion | Resultado </th>')
+            reporte.write('</tr>')
+            reporte.write('</thead>')
+            reporte.write('<tbody>')
+            contador4 = -1
             for obj in objectos:
-                if obj.ordenar() != None:
-                    reporte.write('<p>' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(obj.ordenar) + '</p>')
+                reporte.write('<tr>')
+                reporte.write('<th class="row">' + str(contador4 + 1) + '</th>')
+                contador4 += 1
+                if obj.instruccion1 is True:
+                    if obj.instruccion2 != None:
+                        reporte.write('<td>' + memoria[contador4] + ' | ' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(ordenar(obj)) + 'BUSQUEDA ' + str(obj.instruccion2) + ' = ' + buscar(obj) + '</td>')
+                    elif obj.instruccion2 == None:
+                        reporte.write('<td>' + memoria[contador4] + ' | ' + str(obj.id) + ': ' + 'ORDENADOS = ' + str(ordenar(obj))+ '</td>')
+
+                elif obj.instruccion2 != None:
+                    reporte.write('<td>' + memoria[contador4] + ' | ' + str(obj.id) + ': ' + 'BUSQUEDA ' + str(obj.instruccion2) + ' = ' + buscar(obj) + '</td>')
+
                 else:
-                    if obj.buscar() == None:
-                        continue
-                    else:
-                        reporte.write('<p>' + str(obj.id) + ': ' + 'BUSQUEDA' +  str(obj.instruccion2) + ' = '  + str(obj.buscar) + '</p>')
-            reporte.write('</center>')
-            reporte.write('</body>')
-            reporte.write('</html>')
+                    continue
+                reporte.write('</tr>')
+
+            reporte.write('</tbody>')
+            reporte.write('</table>')
+            reporte.write('</center>' + '\n')
+            reporte.write('</body>' + '\n')
+            reporte.write('</html>' + '\n')
 
             webbrowser.open('reporte.html')
 
@@ -180,8 +269,5 @@ while True:
         print('rodrigito49@gmail.com')
         print('Lenguajes Formales y de Programacion')
         print('=============')
+        quit()
 
-    else:
-        print('Comando desconocido')
-        continue
-"""
